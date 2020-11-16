906ba35c5f79b161a1c1fd976b54d47280f126a6
afigurekim <afigurekim@gmail.com>
2020-11-16 11:45:21 +0900

    status 수정

--- src/frontend/src/pages/Status.vue
+++ src/frontend/src/pages/Status.vue
@@ -5,43 +5,45 @@
     </div>
     <q-list class="q-mb-xl">
       <q-separator />
-      <div v-for="(dealOne, index) in dealList" :key="index">
-        <q-item class="q-py-md">
-          <q-item-section avatar>
-            <q-avatar square size="4em">
-              <img :src="dealOne.pnMdlImg" />
-            </q-avatar>
-          </q-item-section>
+      <q-infinite-scroll @load="onScrollLoad" :offset="110">
+        <div v-for="(dealOne, index) in dealList" :key="index">
+          <q-item class="q-py-md">
+            <q-item-section avatar>
+              <q-avatar square size="4em">
+                <img :src="dealOne.pnImg" />
+              </q-avatar>
+            </q-item-section>
 
-          <q-item-section>
-            <q-item-label class="text-weight-bold">
-              {{ dealOne.pnMdlNm }}
-            </q-item-label>
-            <q-item-label>
-              최저가
-              <span class="text-h6 text-weight-bold">
-                <span class="text-primary">
-                  {{ dealOne.pnMinPrice | showMeTheMoney }}
+            <q-item-section>
+              <q-item-label class="text-weight-bold">
+                {{ dealOne.pnMdlNm }}
+              </q-item-label>
+              <q-item-label>
+                최저가
+                <span class="text-h6 text-weight-bold">
+                  <span class="text-primary">
+                    {{ dealOne.pnMinPrice | showMeTheMoney }}
+                  </span>
+                  <q-icon name="delete_outline" color="black" />
                 </span>
-                <q-icon name="delete_outline" color="black" />
-              </span>
-            </q-item-label>
-            <q-item-label caption lines="2" class="text-black">
-              {{ dealOne.mntCarr }} / {{ dealOne.pnRegDis }} /
-              {{ dealOne.pnMntRtNm }}
-            </q-item-label>
-          </q-item-section>
+              </q-item-label>
+              <q-item-label caption lines="2" class="text-black">
+                {{ dealOne.mntCarr }} / {{ dealOne.pnRegDis }} /
+                {{ dealOne.pnMntRtNm }}
+              </q-item-label>
+            </q-item-section>
 
-          <q-item-section side>
-            <q-icon
-              name="chevron_right"
-              color="black"
-              @click="dealClick(dealOne.dealNo)"
-            />
-          </q-item-section>
-        </q-item>
-        <q-separator />
-      </div>
+            <q-item-section side>
+              <q-icon
+                name="chevron_right"
+                color="black"
+                @click="dealClick(dealOne.dealNo)"
+              />
+            </q-item-section>
+          </q-item>
+          <q-separator />
+        </div>
+      </q-infinite-scroll>
     </q-list>
   </div>
 </template>
@@ -51,61 +53,11 @@ export default {
   name: "PageStatus",
   data() {
     return {
+      pageInfo: {},
       /** DEAL 리스트 */
-      dealList: [
-        {
-          dealNo: "100000000001",
-          dealDt: "2020-11-11 00:00:00",
-          pnMdlImg: "/images/phone_thumbnail.jpg",
-          pnMdlNm: "삼성 갤럭시 S20",
-          mntCarr: "SKT",
-          pnRegDis: "번호이동",
-          pnMntRtNm: "5GX 플래티넘",
-          pnMinPrice: "250000"
-        },
-        {
-          dealNo: "100000000002",
-          dealDt: "2020-11-11 00:00:00",
-          pnMdlImg: "/images/phone_thumbnail.jpg",
-          pnMdlNm: "삼성 갤럭시 S20",
-          mntCarr: "SKT",
-          pnRegDis: "번호이동",
-          pnMntRtNm: "5GX 플래티넘",
-          pnMinPrice: "250000"
-        },
-        {
-          dealNo: "100000000003",
-          dealDt: "2020-11-11 00:00:00",
-          pnMdlImg: "/images/phone_thumbnail.jpg",
-          pnMdlNm: "삼성 갤럭시 S20",
-          mntCarr: "SKT",
-          pnRegDis: "번호이동",
-          pnMntRtNm: "5GX 플래티넘",
-          pnMinPrice: "250000"
-        },
-        {
-          dealNo: "100000000004",
-          dealDt: "2020-11-11 00:00:00",
-          pnMdlImg: "/images/phone_thumbnail.jpg",
-          pnMdlNm: "삼성 갤럭시 S20",
-          mntCarr: "SKT",
-          pnRegDis: "번호이동",
-          pnMntRtNm: "5GX 플래티넘",
-          pnMinPrice: "250000"
-        },
-        {
-          dealNo: "100000000005",
-          dealDt: "2020-11-11 00:00:00",
-          pnMdlImg: "/images/phone_thumbnail.jpg",
-          pnMdlNm: "삼성 갤럭시 S20",
-          mntCarr: "SKT",
-          pnRegDis: "번호이동",
-          pnMntRtNm: "5GX 플래티넘",
-          pnMinPrice: "250000"
-        }
-      ],
+      dealList: [],
       /** $cf.call 파라미터 예제 */
-      paramTest: {
+      listParam: {
         email: "clientTest@gmail.com",
         page: "1"
       }
@@ -113,12 +65,13 @@ export default {
   },
   mounted() {
     /** $cf.call 예제 */
-    this.$cf.call(
-      process.env.API + "/api/deal/list",
-      this.paramTest,
-      this.cbTest,
-      true
-    );
+    // this.$cf.call(
+    //   process.env.API + "/api/deal/list",
+    //   this.listParam,
+    //   this.listCb,
+    //   true
+    // );
+    // this.getList("1");
   },
   methods: {
     /** > 클릭 이벤트 */
@@ -126,28 +79,50 @@ export default {
       this.$router.push({ path: "/ongoing/" + dealNo });
     },
     /** $cf.call 콜백 함수 예제 */
-    cbFunction(response) {
-      let responseColor = "warning";
-      let responseTextColor = "dark";
-      if (response) {
-        responseColor = "positive";
-        responseTextColor = "white";
-      }
-      this.$store.commit("setNotification", {
-        color: responseColor,
-        textColor: responseTextColor,
-        message: response + "",
-        caption: "정상적인 결과 출력"
-      });
+    // cbFunction(response) {
+    //   let responseColor = "warning";
+    //   let responseTextColor = "dark";
+    //   if (response) {
+    //     responseColor = "positive";
+    //     responseTextColor = "white";
+    //   }
+    //   this.$store.commit("setNotification", {
+    //     color: responseColor,
+    //     textColor: responseTextColor,
+    //     message: response + "",
+    //     caption: "정상적인 결과 출력"
+    //   });
+    // },
+    /** list 호출 함수 */
+    getList(page) {
+      this.$cf.call(
+        process.env.API + "/api/deal/list",
+        {
+          email: "clientTest@gmail.com",
+          page: page
+        },
+        this.listCb,
+        true
+      );
     },
-    cbTest(response) {
+    listCb(response) {
       console.log(response);
-      this.$store.commit("setNotification", {
-        color: "positive",
-        textColor: "white",
-        message: "SUCC",
-        caption: "정상적인 결과 출력"
-      });
+      this.pageInfo = response.pageInfo;
+      for (let n in response.dealList) {
+        this.dealList.push(response.dealList[n]);
+      }
+    },
+    /** infinite scroll 이벤트 */
+    onScrollLoad(index, done) {
+      setTimeout(() => {
+        if (index === 1) {
+          this.getList(index + "");
+          done();
+        } else if (this.pageInfo.hasNextPage) {
+          this.getList(this.pageInfo.nextPage + "");
+          done();
+        }
+      }, 1);
     }
   }
 };
