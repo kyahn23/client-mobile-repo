4c72aae089381c6897728c0a3cea5cfbbb50f9a4
kyahn23 <kyahn23@gmail.com>
2020-11-16 18:50:28 +0900

    최저가조회 페이지 작업중

--- src/main/java/com/pentas/clientmobile/controller/PriceRestController.java
+++ src/main/java/com/pentas/clientmobile/controller/PriceRestController.java
@@ -1,14 +1,50 @@
 package com.pentas.clientmobile.controller;
 
 
+import com.pentas.clientmobile.common.module.mybatis.paginator.domain.PageList;
+import com.pentas.clientmobile.common.module.util.DevMap;
+import com.pentas.clientmobile.service.PriceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/price")
 public class PriceRestController {
 
+    private final PriceService priceService;
 
+    @PostMapping("/phoneList")
+    public DevMap phoneList(@RequestBody DevMap param) {
+        DevMap rslt = new DevMap();
+        List<DevMap> resultList = null;
+
+        resultList = priceService.phoneList(param);
+        rslt.put("phoneList", resultList);
+        return rslt;
+    }
+
+    @PostMapping("/mntRtList")
+    public DevMap mntRtList(@RequestBody DevMap param) {
+        DevMap rslt = new DevMap();
+        List<DevMap> resultList = null;
+
+        resultList = priceService.mntRtList(param);
+        rslt.put("mntRtList", resultList);
+        return rslt;
+    }
+
+    @PostMapping("/priceList")
+    public DevMap priceList(@RequestBody DevMap param) {
+        DevMap result = new DevMap();
+        PageList<DevMap> listPage = priceService.priceList(param);
+        result.put("priceList", listPage);
+        result.put("pageInfo", listPage.getPaginator());
+        return result;
+    }
 }
--- src/main/java/com/pentas/clientmobile/service/PriceService.java
+++ src/main/java/com/pentas/clientmobile/service/PriceService.java
@@ -1,9 +1,45 @@
 package com.pentas.clientmobile.service;
 
-import lombok.RequiredArgsConstructor;
+import com.pentas.clientmobile.common.CmmnDao;
+import com.pentas.clientmobile.common.module.mybatis.paginator.domain.PageBounds;
+import com.pentas.clientmobile.common.module.mybatis.paginator.domain.PageList;
+import com.pentas.clientmobile.common.module.util.DevMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+import java.util.List;
+
 @Service
 public class PriceService {
+
+    @Autowired
+    CmmnDao cmmnDao;
+
+    /**
+     * 제조사별 스마트폰 리스트
+     * @param param
+     * @return
+     */
+    public List<DevMap> phoneList(DevMap param) {
+        return cmmnDao.selectList("clientmobile.price.phoneList", param);
+    }
+
+    /**
+     * 선택한 모델에 따른 선택 가능 요금제
+     * @param param
+     * @return
+     */
+    public List<DevMap> mntRtList(DevMap param) {
+        return cmmnDao.selectList("clientmobile.price.mntRtList", param);
+    }
+
+
+    public PageList<DevMap> priceList(DevMap param) {
+        int page = Integer.parseInt(param.getString("page"));
+        int pageSize = 10;
+
+        PageBounds pageBounds = new PageBounds(page, pageSize);
+
+        return cmmnDao.selectListPage("clientmobile.price.priceList", param, pageBounds);
+    }
 }
